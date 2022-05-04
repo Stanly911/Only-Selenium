2407776e9ee1f46374788d1aaef8ec4cec1269de
Stanly911 <stanlymallick@outlook.com>
2022-05-04 19:40:36 +0530

    Hello

--- src/handlingWebElements/FileUpload.java
+++ src/handlingWebElements/FileUpload.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FileUpload 
 {
-	
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException  
 	{
 		WebDriver driver = new ChromeDriver();
